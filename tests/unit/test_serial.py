686600fb66d26f7792b99330f307f0bf6887d9d1
Mac Chapman <mac@veryhappythings.co.uk>
2015-10-07 10:41:08 +0100

    Move tests onto automagical ports

--- tests/unit/test_serial.py
+++ tests/unit/test_serial.py
@@ -9,19 +9,19 @@ class TestMultipleServers(unittest.TestCase):
     def assert_success(self):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect('127.0.0.1', port=3000)
+        ssh.connect('127.0.0.1', port=10000)
         channel = ssh.invoke_shell()
         fileobj = channel.makefile('rw')
         self.assertEqual(fileobj.readline(), 'success\r\n')
         ssh.close()
 
     def test_one_after_another(self):
-        with sshim.Server(success, port=3000):
+        with sshim.Server(success, port=10000):
             self.assert_success()
 
-        with sshim.Server(success, port=3000):
+        with sshim.Server(success, port=10000):
             self.assert_success()
 
     def test_another_server(self):
-        with sshim.Server(success, port=3000):
+        with sshim.Server(success, port=10000):
             self.assert_success()
